94228e6db2f0876ac1bb49cec464d6593927ab8d
JuanCastroB <78882174+JuanCastroB@users.noreply.github.com>
2021-04-14 16:02:04 -0500

    Initial commit

---
+++ data/PlantasCENIT_6.js
@@ -0,0 +1,11 @@
+var json_PlantasCENIT_6 = {
+"type": "FeatureCollection",
+"name": "PlantasCENIT_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Planta Buenaventura" }, "geometry": { "type": "Point", "coordinates": [ -77.062410185502728, 3.884385285878923 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Planta Yumbo" }, "geometry": { "type": "Point", "coordinates": [ -76.485202030700819, 3.579143166271358 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Planta Cartago" }, "geometry": { "type": "Point", "coordinates": [ -75.916135695066117, 4.725266372691722 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Planta Dagua" }, "geometry": { "type": "Point", "coordinates": [ -76.677168061304286, 3.731920126313554 ] } }
+]
+}
---
+++ data/Pluviometros_7.js
@@ -0,0 +1,11 @@
+var json_Pluviometros_7 = {
+"type": "FeatureCollection",
+"name": "Pluviometros_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "Estacion": "Somberillo", "IdEstacion": "630", "Latitud": 3.79241, "Longitud": -76.77564, "AcumMM": 92.1 }, "geometry": { "type": "Point", "coordinates": [ -76.775639999953142, 3.792410000363704 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2", "Estacion": "Loboguerrero", "IdEstacion": "670", "Latitud": 3.761772, "Longitud": -76.67305, "AcumMM": 20.2 }, "geometry": { "type": "Point", "coordinates": [ -76.673050000152202, 3.761772000232884 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "3", "Estacion": "Pavas", "IdEstacion": "669", "Latitud": 3.681231, "Longitud": -76.578281, "AcumMM": 5.9 }, "geometry": { "type": "Point", "coordinates": [ -76.578280999561215, 3.681230999625586 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "4", "Estacion": "Montanita", "IdEstacion": "631", "Latitud": 3.65204, "Longitud": -76.53225, "AcumMM": 3.2 }, "geometry": { "type": "Point", "coordinates": [ -76.53224999972538, 3.652039999945348 ] } }
+]
+}
